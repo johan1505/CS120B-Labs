1f3666ef5e52b9f03574ff23ad61efe5353b474d
Johan Guzman <jguzm022@ucr.edu>
2019-04-10 11:39:25 -0700

    Lab4_part2 FIRST COMMIT

--- Lab3/jguzm022_lab4_part2/jguzm022_lab4_part2/main.c
+++ Lab3/jguzm022_lab4_part2/jguzm022_lab4_part2/main.c
@@ -7,12 +7,97 @@
 
 #include <avr/io.h>
 
+enum IDC_States { IDC_SMStart, INIT, INCREMENT, DECREMENT, WAIT, RESET  } IDC_State;
+
+void TickFct_Latch()
+{
+	unsigned char tempA0 = PINA & 0x01;
+	unsigned char tempA1 = PINA & 0x02;
+	switch(IDC_State) {   // Transitions
+		case IDC_SMStart:  // Initial transition
+			IDC_State = INIT;
+			break;
+
+		case INIT:
+			if (tempA0 && !tempA1) {
+				IDC_State = INCREMENT;
+			}
+			else if (tempA1 && !tempA0) {
+				IDC_State = DECREMENT;
+			}
+			else if (tempA0 && tempA1){
+				IDC_State = RESET;
+			}
+			else {
+				IDC_State = INIT;
+			}
+			break;
+
+		case INCREMENT:
+			IDC_State = WAIT;
+			break;
+			
+		case DECREMENT:
+			IDC_State = WAIT;
+			break;
+			
+		case WAIT:
+			if (tempA0 && !tempA1 && (PORTC < 9)){
+				IDC_State = INCREMENT;	
+			}
+			else if (tempA1 && !tempA0 && (PORTC > 0)){
+				IDC_State = DECREMENT;	
+			}
+			else if (tempA0 && tempA1){
+				IDC_State = RESET;
+			}
+			else if ((tempA0 && (PINC >= 9)) || (!tempA0 && !tempA1) || (tempA1 && (PORTC == 0))){
+				IDC_State = WAIT;
+			}
+			break;
+			
+		case RESET:
+			IDC_State = WAIT;
+			break;
+			
+		default:
+			IDC_State = IDC_SMStart;
+			break;
+	} // Transitions
+
+	switch(IDC_State) {   // State actions
+		case INIT:
+			PORTC = 0x07;
+			break;
+
+		case INCREMENT:
+			PORTC = PORTC + 1;
+			break;
+		
+		case DECREMENT:
+			PORTC = PORTC - 1;
+			break;
+		
+		case WAIT:
+			break;
+			
+		case RESET:
+			PORTC = 0x00;
+			break;
+			
+		default:
+		break;
+	} // State actions
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	IDC_State = IDC_SMStart;
     while (1) 
     {
+		TickFct_Latch();
     }
 }
 
