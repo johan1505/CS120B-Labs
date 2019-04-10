831bf955b6319bf78cd053948516c93e8228ca9f
Johan Guzman <jguzm022@ucr.edu>
2019-04-10 11:41:48 -0700

    Moving lab4 parts 1 - 3

---
+++ Lab4/jguzm022_lab4_part3/jguzm022_lab4_part3/main.c
@@ -0,0 +1,18 @@
+/*
+ * jguzm022_lab4_part3.c
+ *
+ * Created: 4/10/2019 11:40:08 AM
+ * Author : joan1
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
