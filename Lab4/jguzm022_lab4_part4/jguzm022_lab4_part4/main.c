720649c222e4ab0c1d49d2c8a7c541125d8bad8c
Johan Guzman <jguzm022@ucr.edu>
2019-04-10 17:21:09 -0700

    Lab4 part 3 fixes

---
+++ Lab4/jguzm022_lab4_part4/jguzm022_lab4_part4/main.c
@@ -0,0 +1,18 @@
+/*
+ * jguzm022_lab4_part4.c
+ *
+ * Created: 4/10/2019 5:20:13 PM
+ * Author : joan1
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
